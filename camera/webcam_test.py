3284d96f13c390980683fd78807e7208d71ff5da
Evan <127327512+Zesty313@users.noreply.github.com>
2025-10-01 19:11:15 -0400

    Update webcam_test.py
    Added gstream pipeline for jetson interface

--- camera/webcam_test.py
+++ camera/webcam_test.py
@@ -2,8 +2,33 @@
 
 import cv2
 
-def main():
-
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+    
     cap = cv2.VideoCapture(0)
     
     if not cap.isOpened():
@@ -12,7 +37,7 @@ def main():
 
     print("Webcam is running")
 
-    while True:
+    else:
         ret, frame = cap.read()
         if not ret:
             print("Failed to grab frame")
@@ -25,6 +50,3 @@ def main():
     
     cap.release()
     cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
